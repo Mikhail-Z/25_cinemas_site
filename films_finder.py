1ce7fa5d62de87b55f0864c0f76ecc81d877aca9
mikhail-z <09041996a>
2018-05-16 23:12:17 +0300

    films information is searched by multiple threads

--- films_finder.py
+++ films_finder.py
@@ -1,53 +1,87 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import threading
+from math import ceil
 
 
 def fetch_page(url):
     return requests.get(url).text
 
 
-def parse_film_raw_info(raw_film_info):
-    raw_main_film_info = raw_film_info.find("a", "card__link")
-    name = raw_main_film_info.find("h3", "card__title").text.strip()
+def get_film_description(raw_film_info):
     try:
-        description = raw_main_film_info.find("p", "card__verdict").text.strip()
+        description = raw_film_info.find("p", "card__verdict").text.strip()
     except AttributeError:
         description = ""
-    image_link = raw_main_film_info.find("img", "card__image")["src"]
+    return description
+
+
+def get_film_score(raw_film_info):
+    default_score = 5.0
     try:
         score = float(raw_film_info.find("div", "rating__selector-value").text.strip())
     except AttributeError:
-        score = 5.0
+        score = default_score
+    return score
 
+
+def get_cinemas_num_with_film(raw_film_info):
     cinemas_num_str = raw_film_info.find("div", {"itemprop": "location"}).find(
         "meta", {"itemprop": "name"})["content"]
     cinemas_num = int(re.search(r"\d+", cinemas_num_str).group())
+    return cinemas_num
 
-    genre = raw_film_info.find("a", "card__badge").text.strip()
+
+def get_film_page_on_afisha(raw_film_info):
     afisha_url = "https://www.afisha.ru"
-    film_on_afisha_url = "{}{}".format(afisha_url, raw_main_film_info["href"])
-    return {
-        "name": name,
-        "description": description,
-        "image": image_link,
-        "score": score,
-        "cinemas_num": cinemas_num,
-        "genre": genre,
-        "url_on_afisha": film_on_afisha_url
-    }
+    film_on_afisha_url = "{}{}".format(afisha_url, raw_film_info["href"])
+    return film_on_afisha_url
+
+
+def parse_films_raw_info(raw_films_info, film_info, fst_idx, last_idx):
+    for raw_film_info in raw_films_info[fst_idx: last_idx]:
+        raw_film_main_info = raw_film_info.find("a", "card__link")
+        name = raw_film_main_info.find("h3", "card__title").text.strip()
+        description = get_film_description(raw_film_main_info)
+        image_link = raw_film_main_info.find("img", "card__image")["src"]
+        score = get_film_score(raw_film_info)
+        cinemas_num = get_cinemas_num_with_film(raw_film_info)
+        genre = raw_film_info.find("a", "card__badge").text.strip()
+        film_on_afisha_url = get_film_page_on_afisha(raw_film_main_info)
+        cinemas_num_when_film_is_arthouse = 30
+        if cinemas_num > cinemas_num_when_film_is_arthouse:
+            film_info.append({
+                "name": name,
+                "description": description,
+                "image": image_link,
+                "score": score,
+                "cinemas_num": cinemas_num,
+                "genre": genre,
+                "url_on_afisha": film_on_afisha_url
+            })
 
 
 def parse_afisha_list(raw_html):
     soup = BeautifulSoup(raw_html, "html.parser")
     raw_info_about_films = soup.find_all("div", "card cards-grid__item")
-    films_info = []
-    cinemas_num_when_film_is_arthouse = 30
 
-    for i, raw_film_info in enumerate(raw_info_about_films):
-        film_info = parse_film_raw_info(raw_film_info)
-        if film_info["cinemas_num"] > cinemas_num_when_film_is_arthouse:
-            films_info.append(film_info)
+    threads_num = 8
+    films_num = len(raw_info_about_films)
+    films_per_thread = int(ceil(films_num / threads_num))
+    threads = []
+    films_info = []
+
+    for fst_thread_film_idx in range(0, films_num, films_per_thread):
+        last_thread_film_idx = min(fst_thread_film_idx+films_per_thread, films_num)
+        thread = threading.Thread(
+            target=parse_films_raw_info,
+            args=(raw_info_about_films, films_info, fst_thread_film_idx, last_thread_film_idx,)
+        )
+        thread.start()
+        threads.append(thread)
+    for thread in threads:
+        thread.join()
     return films_info
 
 
